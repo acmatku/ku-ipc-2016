ca1426b5fb197b416df351cda80956136241b376
Ethan Ward <ethanandrewward@gmail.com>
2016-04-16 13:07:15 -0500

    Remove test code

--- Potential/Hide_text_in_images/Solver.java
+++ Potential/Hide_text_in_images/Solver.java
@@ -3,32 +3,31 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.io.*;
 public class Solver {
-	
+
 	enum States{W,Z,O,I};
-	
+
 	public static void main(String[] args) throws IOException {
-		
+
 		Path filePath = Paths.get("input1.txt");
 		Scanner reader = new Scanner(filePath);
 		String message = "";
-		String message2 = "";
+		String translate = "";
 		States state;
 		state = States.W;
-		int test = 0;
 
 		while(reader.hasNext()) {
 			test++;
 			if(reader.hasNextInt()) {
 				String value = String.valueOf(reader.nextInt());
 				value = value.substring(value.length()-1);
-				
+
 				switch(state) {
 				case W:	if(value.startsWith("0")) {
 						state = States.Z;
 						message = message + value;
 					}
 					break;
-					
+
 				case Z:	if(value.startsWith("1")) {
 						state = States.O;
 						message = message + value;
@@ -36,7 +35,7 @@ public class Solver {
 					else {state = States.W;
 							message = "";}
 					break;
-					
+
 				case O:	if(value.startsWith("1") || value.startsWith("0")) {
 						message = message + value;
 						state = States.I;
@@ -44,30 +43,26 @@ public class Solver {
 					else {state = States.W;
 							message = "";}
 					break;
-					
+
 				case I:	if(!(value.startsWith("1")|| value.startsWith("0"))) {
 						state = States.W;
-						
+
 					}
 					else {
 						message = message + value;
 						if(message.length()== 8) {
-							message2 += (char)Integer.parseInt(message,2);
+							translate += (char)Integer.parseInt(message,2);
 							message = "";
 						}
 					}
 					break;
-					
+
 				}
-				
+
 			}
 			else {reader.next();}
 		}
-		System.out.println(message2);
-		System.out.println(test);
-		
-		
-
+		System.out.println(translate);
 	}
 
 }
