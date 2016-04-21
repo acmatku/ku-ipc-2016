5d93be3716382d3385e752a332df6af8d5fb2fd0
aleksanderesk <aleksanderesk@gmail.com>
2016-04-20 20:43:51 -0500

    fixing solution, changing description, new test file

--- 02-Medium/War/Clojure/cardus-belli.clj
+++ 02-Medium/War/Clojure/cardus-belli.clj
@@ -11,51 +11,43 @@
   (map #(Integer/parseInt %) (clojure.string/split num-str #" ")))
 
 (defn war
-  [p1-deck p2-deck p1-thresholds p2-thresholds war-stack]
-  (cond (< (count p1-deck) 3) ['() p2-deck p1-thresholds p2-thresholds]
-        (< (count p2-deck) 3) [p1-deck '() p1-thresholds p2-thresholds]
+  [p1-deck p2-deck war-stack]
+  (println (clojure.string/join " " ["war" (count p1-deck) (count p2-deck) (count war-stack)]))
+  (cond (< (count p1-deck) 4) ['() (concat p2-deck p1-deck war-stack)]
+        (< (count p2-deck) 4) [(concat p1-deck p2-deck war-stack) '()]
         :else (let [war-stack' (concat war-stack (take 3 p1-deck) (take 3 p2-deck))
-                    p1-deck' (concat (nthrest p1-deck 3))
-                    p2-deck' (concat (nthrest p2-deck 3))
+                    p1-deck' (nthrest p1-deck 3)
+                    p2-deck' (nthrest p2-deck 3)
                     p1-card (first p1-deck')
                     p2-card (first p2-deck')]
                 (cond (> p1-card p2-card) [(concat (rest p1-deck') (list p1-card p2-card) war-stack')
-                                           (rest p2-deck')
-                                           p1-thresholds
-                                           p2-thresholds]
+                                           (rest p2-deck')]
                       (< p1-card p2-card) [(rest p1-deck')
-                                           (concat (rest p2-deck') (list p2-card p1-card) war-stack')
-                                           p1-thresholds
-                                           p2-thresholds]
-                      :else (war p1-deck' p2-deck' p1-thresholds p2-thresholds war-stack')))))
+                                           (concat (rest p2-deck') (list p2-card p1-card) war-stack')]
+                      :else (war (rest p1-deck') (rest p2-deck') (concat (list p1-card p2-card) war-stack'))))))
 
 (defn game
-  [p1-deck p2-deck p1-thresholds p2-thresholds]
-  (loop [state [p1-deck p2-deck p1-thresholds p2-thresholds]]
-    (let [[p1-d p2-d p1-t p2-t] state]
+  [p1-deck p2-deck]
+  (loop [state [p1-deck p2-deck]]
+    (println (clojure.string/join " " ["battle" (count (first state)) (count (second state))]))
+    (let [[p1-d p2-d] state]
       (if (and (seq p1-d) (seq p2-d))
         (let [p1-card (first p1-d)
               p2-card (first p2-d)]
           (cond
             (> p1-card p2-card) (recur [(concat (rest p1-d) (list p1-card p2-card))
-                                        (rest p2-d)
-                                        p1-t
-                                        p2-t])
+                                        (rest p2-d)])
             (< p1-card p2-card) (recur [(rest p1-d)
-                                        (concat (rest p2-d) (list p2-card p1-card))
-                                        p1-t
-                                        p2-t])
-            :else (recur (war p1-d p2-d p1-t p2-t '()))))
+                                        (concat (rest p2-d) (list p2-card p1-card))])
+            :else (recur (war (rest p1-d) (rest p2-d) (list p1-card p2-card)))))
         (if (seq p1-d)
-          "Player 1"
-          "Player 2")))))
+          ["PLAYER 1" (count p1-d) p1-d]
+          ["PLAYER 2" (count p2-d) p2-d])))))
 
 (defn main
   []
   (let [p1-cards (str->cards (read-line))
-        p2-cards (str->cards (read-line))
-        p1-thresholds (str->nums (read-line))
-        p2-thresholds (str->nums (read-line))]
-    (println (game p1-cards p2-cards p1-thresholds p2-thresholds))))
+        p2-cards (str->cards (read-line))]
+    (println (game p1-cards p2-cards))))
 
 (time (main))
