672c50c20b592228d109c9d9ac411d7d07fb3f9f
Ethan Ward <ethanandrewward@gmail.com>
2016-04-21 17:02:16 -0500

    Add updated War solution

---
+++ 02-Medium/War/main.cpp
@@ -0,0 +1,148 @@
+#ifndef MAIN_CPP
+#define MAIN_CPP
+
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <queue>
+
+int convert(char card) {
+    switch(card) {
+      case 'T': return(10); break;
+      case 'J': return(11); break;
+      case 'Q': return(12); break;
+      case 'K': return(13); break;
+      case 'A': return(14); break;
+      default: return(card - '0'); break;
+    }
+}
+
+//
+void war(std::queue<int> &player1, std::queue<int> &player2, std::queue<int> &warStack) {
+  int size;
+  //Checks if each player's hand is smaller than 4, which would prevent 3 cards being moved to the back of the deck
+  //If this has been called on multiple wars, increases the needed hand size by 3 each time
+  if(player1.size() < 4) {
+    size = player1.size();
+    for(int i = 0; i < size; i++) {
+      player1.pop();
+    }
+    return;
+  } else if (player2.size() < 4) {
+    size = player2.size();
+    for(int i = 0; i < size; i++) {
+      player2.pop();
+    }
+    return;
+  }
+  //If the hand size is valid
+  else {
+    //Put the next 3 cards to the back of each player's deck, then delete the unneeded values.
+    for(int i = 0; i < 3;i++) {
+      warStack.push(player1.front());
+      warStack.push(player2.front());
+      player1.pop();
+      player2.pop();
+    }
+    //If player 1 wins, put the winning cards on the bottom of their deck and delete the unneeded cards.
+    if(player1.front() > player2.front()) {
+      warStack.push(player1.front());
+      warStack.push(player2.front());
+      player1.pop();
+      player2.pop();
+      size = warStack.size();
+      for(int i = 0; i < size; i++) {
+        player1.push(warStack.front());
+        warStack.pop();
+      }
+      return;
+
+    } else if(player1.front() < player2.front()) {
+      warStack.push(player2.front());
+      warStack.push(player1.front());
+      player1.pop();
+      player2.pop();
+      size = warStack.size();
+      for(int i = 0; i < size; i++) {
+        player2.push(warStack.front());
+        warStack.pop();
+      }
+      return;
+    }
+    else {
+      warStack.push(player1.front());
+      warStack.push(player2.front());
+
+      player1.pop();
+      player2.pop();
+      war(player1,player2,warStack);
+    }
+}
+}
+
+int main() {
+//Vectors representing each player's hands
+std::string deck1;
+std::string deck2;
+std::string test;
+std::queue<int> player1;
+std::queue<int> player2;
+std::queue<int> warStack;
+char c;
+
+//Put the input into the needed vectors
+
+std::getline(std::cin, deck1);
+std::istringstream iss (deck1);
+std::getline(std::cin, deck2);
+std::istringstream iss2 (deck2);
+
+while(std::getline(iss,test,' ')) {
+  player1.push(convert(test[0]));
+}
+while(std::getline(iss2,test,' ')) {
+  player2.push(convert(test[0]));
+}
+
+//std::cout << deck1 << "\n" << deck2;
+//Run the game until one player has no cards
+while(!player1.empty() || !player2.empty()) {
+std::cout << player1.size() << player2.size() << "\n";
+  //If player 1 wins, put both cards at the back of their deck and then delete each player's first cards
+  if(player1.front() > player2.front()) {
+    player1.push(player1.front());
+    player1.push(player2.front());
+    player1.pop();
+    player2.pop();
+  //If player 2 wins, put both cards at the back of their deck and then delete each player's first cards
+  } else if(player1.front() < player2.front()) {
+    player2.push(player2.front());
+    player2.push(player1.front());
+    player1.pop();
+    player2.pop();
+  //If there is a tie, do a war
+  } else {
+    warStack.push(player1.front());
+    warStack.push(player2.front());
+
+    player1.pop();
+    player2.pop();
+    war(player1,player2,warStack);
+  }
+  if(player1.empty()) {
+    std::cout << "Player 1 wins";
+    break;
+  }
+  else if(player2.empty()){
+    std::cout << "Player 2 wins";
+    break;
+  }
+
+}
+
+
+// return 0;
+};
+
+#endif
