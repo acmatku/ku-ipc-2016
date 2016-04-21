a8a821a8d3a539451a4194fee40feec1a6cd17a4
Ethan Ward <ethanandrewward@gmail.com>
2016-04-20 21:13:06 -0500

    Add changes to war solution

--- 02-Medium/War/main.cpp
+++ 02-Medium/War/main.cpp
@@ -17,91 +17,113 @@ int convert(char card) {
 }
 
 //
-void war(std::vector<int> &player1, std::vector<int> &player2, int &start) {
+void war(std::vector<int> &player1, std::vector<int> &player2, std::vector<int> &warStack) {
   //Checks if each player's hand is smaller than 4, which would prevent 3 cards being moved to the back of the deck
   //If this has been called on multiple wars, increases the needed hand size by 3 each time
-  if(player1.size() <= 3+start) {
+  if(player1.size() <= 3) {
     player1.clear();
     return;
-  } else if (player2.size() <= 3+start) {
+  } else if (player2.size() <= 3) {
     player2.clear();
     return;
   }
   //If the hand size is valid
   else {
     //Put the next 3 cards to the back of each player's deck, then delete the unneeded values.
-    for(int i = 1+start; i < 4+start;i++) {
-      player1.push_back(player1[i]);
-      player2.push_back(player2[i]);
-      player1.erase(player1.begin()+1+start);
-      player2.erase(player1.begin()+1+start);
+    for(int i = 0; i < 3;i++) {
+      warStack.push_back(player1[i]);
+      warStack.push_back(player2[i]);
+      player1.erase(player1.begin());
+      player2.erase(player1.begin());
     }
     //If player 1 wins, put the winning cards on the bottom of their deck and delete the unneeded cards.
-    if(player1[1+start] > player2[1+start]) {
-      player1.push_back(player1[1+start]);
-      player1.push_back(player2[1+start]);
-      player1.erase(player1.begin()+1+start);
-      player2.erase(player2.begin()+1+start);
-      start = 0;
+    if(player1[0] > player2[0]) {
+      for(int i = 0; i < warStack.size(); i++) {
+        player1.push_back(warStack[0]);
+        warStack.erase(warStack.begin());
+      }
+      player1.push_back(player1[0]);
+      player1.push_back(player2[0]);
+      player1.erase(player1.begin());
+      player2.erase(player2.begin());
       return;
-    //If player 2 wins, put the winning cards on the bottom of their deck and delete the unneeded cards.
-    } else if(player1[1+start] < player2[1+start]) {
-      player2.push_back(player1[1+start]);
-      player2.push_back(player2[1+start]);
-      player2.erase(player2.begin()+1+start);
-      player1.erase(player1.begin()+1+start);
-      start = 0;
+    } else if(player1[0] < player2[0]) {
+      for(int i = 0; i < warStack.size(); i++) {
+        player2.push_back(warStack[0]);
+        warStack.erase(warStack.begin());
+      }
+      player2.push_back(player1[0]);
+      player2.push_back(player2[0]);
+      player1.erase(player1.begin());
+      player2.erase(player2.begin());
       return;
-    //If there is a tie, increase the starting index by 3 and do another war.
     } else {
-      start += 3;
-      war(player1,player2,start);
+      warStack.push_back(player1[0]);
+      warStack.push_back(player2[0]);
+      player1.erase(player1.begin());
+      player2.erase(player2.begin());
+      war(player1,player2,warStack);
+      return;
     }
-  }
-
+}
 }
 
 int main() {
 //Vectors representing each player's hands
+std::string deck1;
+std::string deck2;
 std::vector<int> player1;
 std::vector<int> player2;
-std::ifstream myFile;
-//Int passed by reference used to determine if a war has been called multiple times
-int start;
-
-myFile.open("input1.txt");
+std::vector<int> warStack;
+char c;
 
 //Put the input into the needed vectors
 for(int i = 0; i < 26; i++) {
-  player1.push_back(convert(myFile.get()));
-}
-myFile.ignore();
- for(int i = 0; i < 26; i++) {
-  player2.push_back(convert(myFile.get()));
+  std::cin >> c;
+  player1.push_back(convert(c));
 }
 
+ for(int i = 0; i < 26; i++) {
+  std::cin >> c;
+  std::cout << player1[i];
+  player2.push_back(convert(c));
+  //std::cout << i;
+}
+
+std::cout << "test";
 //Run the game until one player has no cards
 while(!player1.empty() || !player2.empty()) {
+
   //If player 1 wins, put both cards at the back of their deck and then delete each player's first cards
   if(player1[0] > player2[0]) {
     player1.push_back(player1[0]);
     player1.push_back(player2[0]);
     player1.erase(player1.begin());
     player2.erase(player2.begin());
-    start = 0;
   //If player 2 wins, put both cards at the back of their deck and then delete each player's first cards
   } else if(player1[0] < player2[0]) {
-    player2.push_back(player1[0]);
     player2.push_back(player2[0]);
+    player2.push_back(player1[0]);
     player1.erase(player1.begin());
     player2.erase(player2.begin());
-    start = 0;
   //If there is a tie, do a war
   } else {
-    war(player1,player2,start);
+    warStack.push_back(player1[0]);
+    warStack.push_back(player2[0]);
+    player1.erase(player1.begin());
+    player2.erase(player2.begin());
+    war(player1,player2,warStack);
   }
+
 }
 
+if(player1.empty()) {
+  std::cout << "Player 1 wins";
+}
+else {
+  std::cout << "Player 2 wins";
+}
+return 0;
 };
 
 #endif
